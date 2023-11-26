b26e73e7e79a4ecc44df6d7abdfd5c6a5618cd13
quando <quanchopper1234@gmail.com>
2023-11-26 22:11:05 +0700

    update

--- src/pages/Test1/Tree.tsx
+++ src/pages/Test1/Tree.tsx
@@ -1,12 +1,19 @@
-import { useState } from 'react';
-import { Tree, Button } from 'antd';
+import { useEffect, useState } from 'react';
+import { Tree } from 'antd';
 import routes from '../../routes/privateRouter'
 
 const { TreeNode } = Tree;
 
-const PermissionManagement = () => {
+const PermissionManagement = ({value, onChange}:{value?: any, onChange?: any}) => {
+
+    
     const [expandedKeys, setExpandedKeys] = useState([]);
     const [checkedKeys, setCheckedKeys] = useState([]);
+    useEffect(()=>{
+        if(value){
+            setCheckedKeys(value)
+        }
+    },[])
 
     const generateTreeNodes = (routes: any, parentPath = '') => {
         return routes.map((route: any) => {
@@ -36,65 +43,65 @@ const PermissionManagement = () => {
     };
 
     const onCheck = (checkedKeysValue: any) => {
+        onChange(checkedKeysValue)
         setCheckedKeys(checkedKeysValue);
     };
 
-    const handleSavePermissions = () => {
-        const createTreeFromPaths = (paths: any) => {
-            const tree = {};
-            paths.forEach((path: any) => {
-                const pathParts = path.path.split('/');
-                let currentNode = tree as any;
-                pathParts.forEach((part: any, index: any) => {
-                    currentNode[part] = currentNode[part] || {};
-                    currentNode = currentNode[part];
-                    if (index === pathParts.length - 1) {
-                        currentNode.permissions = path.permissions
-                    }
-                });
-            });
-            return tree;
-        };
+    // const handleSavePermissions = () => {
+    //     const createTreeFromPaths = (paths: any) => {
+    //         const tree = {};
+    //         paths.forEach((path: any) => {
+    //             const pathParts = path.path.split('/');
+    //             let currentNode = tree as any;
+    //             pathParts.forEach((part: any, index: any) => {
+    //                 currentNode[part] = currentNode[part] || {};
+    //                 currentNode = currentNode[part];
+    //                 if (index === pathParts.length - 1) {
+    //                     currentNode.permissions = path.permissions
+    //                 }
+    //             });
+    //         });
+    //         return tree;
+    //     };
 
-        const mapTreeToRoutes = (tree: any, routes: any) => {
-            return Object.keys(tree).map((key) => {
-                const route = routes.find((r: any) => r.path === key);
-                if (route) {
-                    if (route?.routes) {
-                        route.routes = mapTreeToRoutes(tree[key], route.routes);
-                    }
-                    else {
-                        route.canAccess = tree[key].permissions
-                        return route
-                    }
-                }
-                return route;
-            }).filter(Boolean);
-        };
+    //     const mapTreeToRoutes = (tree: any, routes: any) => {
+    //         return Object.keys(tree).map((key) => {
+    //             const route = routes.find((r: any) => r.path === key);
+    //             if (route) {
+    //                 if (route?.routes) {
+    //                     route.routes = mapTreeToRoutes(tree[key], route.routes);
+    //                 }
+    //                 else {
+    //                     route.canAccess = tree[key].permissions
+    //                     return route
+    //                 }
+    //             }
+    //             return route;
+    //         }).filter(Boolean);
+    //     };
 
-        const r = checkedKeys.filter((key:any) => key.includes('*')).reduce((result:any, key:any) => {
-            const [path, permission] = key.split('*');
-            if (!result[path]) {
-                result[path] = {
-                    path,
-                    permissions: [],
-                };
-            }
-            if (permission) {
-                result[path].permissions.push(permission);
-            }
-            return result;
-        }, {});
-        const tree = createTreeFromPaths(Object.values(r));
-        console.log(tree)
-        // const mappedRoutes = mapTreeToRoutes(tree, routes.route.routes);
-        console.log(checkedKeys.filter((key:any) => key.includes('*')))
+    //     const r = checkedKeys.filter((key:any) => key.includes('*')).reduce((result:any, key:any) => {
+    //         const [path, permission] = key.split('*');
+    //         if (!result[path]) {
+    //             result[path] = {
+    //                 path,
+    //                 permissions: [],
+    //             };
+    //         }
+    //         if (permission) {
+    //             result[path].permissions.push(permission);
+    //         }
+    //         return result;
+    //     }, {});
+    //     const tree = createTreeFromPaths(Object.values(r));
+    //     console.log(tree)
+    //     // const mappedRoutes = mapTreeToRoutes(tree, routes.route.routes);
+    //     console.log(checkedKeys.filter((key:any) => key.includes('*')))
 
-    };
+    // };
 
     return (
         <div>
-            <h2>Permission Management</h2>
             <Tree
                 checkable
                 onExpand={onExpand}
@@ -106,9 +113,6 @@ const PermissionManagement = () => {
                     {generateTreeNodes(routes.route.routes)}
                 </TreeNode>
             </Tree>
-            <Button type="primary" onClick={handleSavePermissions}>
-                Save Permissions
-            </Button>
         </div>
     );
 };
--- src/pages/users/roles/index.tsx
+++ src/pages/users/roles/index.tsx
@@ -1,91 +1,145 @@
-import { ProList } from '@ant-design/pro-components';
-import { Space, Tag } from 'antd';
-import { useState } from 'react';
+import type { ProColumns } from '@ant-design/pro-components';
+import {
+  EditableProTable,
+} from '@ant-design/pro-components';
+import { Tag } from 'antd';
+import React, { useState } from 'react';
+import PermissionManagement from '../../Test1/Tree';
 
-const defaultData = [
-  {
-    id: '1',
-    name: '语雀的天空',
-    image:
-      'https://gw.alipayobjects.com/zos/antfincdn/efFD%24IOql2/weixintupian_20170331104822.jpg',
-    desc: '我是一条测试的描述',
-  },
-  {
-    id: '2',
-    name: 'Ant Design',
-    image:
-      'https://gw.alipayobjects.com/zos/antfincdn/efFD%24IOql2/weixintupian_20170331104822.jpg',
-    desc: '我是一条测试的描述',
-  },
-  {
-    id: '3',
-    name: '蚂蚁金服体验科技',
-    image:
-      'https://gw.alipayobjects.com/zos/antfincdn/efFD%24IOql2/weixintupian_20170331104822.jpg',
-    desc: '我是一条测试的描述',
-  },
-  {
-    id: '4',
-    name: 'TechUI',
-    image:
-      'https://gw.alipayobjects.com/zos/antfincdn/efFD%24IOql2/weixintupian_20170331104822.jpg',
-    desc: '我是一条测试的描述',
-  },
-];
-
-type DataItem = (typeof defaultData)[number];
-
-const Roles = () => {
-  const [dataSource, setDataSource] = useState<DataItem[]>(defaultData);
-  return (
-    <ProList<DataItem>
-      rowKey="id"
-      headerTitle="基础列表"
-      dataSource={dataSource}
-      showActions="hover"
-      editable={{
-        onSave: async (key, record, originRow) => {
-          console.log(key, record, originRow);
-          return true;
-        },
-      }}
-      onDataSourceChange={setDataSource}
-      metas={{
-        title: {
-          dataIndex: 'name',
-        },
-        avatar: {
-          dataIndex: 'image',
-          editable: false,
-        },
-        description: {
-          dataIndex: 'desc',
-        },
-        subTitle: {
-          render: () => {
-            return (
-              <Space size={0}>
-                <Tag color="blue">Ant Design</Tag>
-                <Tag color="#5BD8A6">TechUI</Tag>
-              </Space>
-            );
-          },
-        },
-        actions: {
-          render: (_, row:any, __, action:any) => [
-            <a
-              onClick={() => {
-                action?.startEditable(row.id);
-              }}
-              key="link"
-            >
-              Sửa
-            </a>,
-          ],
-        },
-      }}
-    />
-  );
+const waitTime = (time: number = 100) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(true);
+    }, time);
+  });
 };
 
-export default Roles
+type DataSourceType = {
+  id: React.Key;
+  title?: string;
+  labels?: string[];
+  children?: DataSourceType[];
+};
+
+const colors = ['bg-indigo-500', 'bg-green-500', 'bg-yellow-500', 'bg-red-500']
+const TagList: React.FC<{
+  value?: [];
+  onChange?: (
+    value: [],
+  ) => void;
+}> = ({ value, onChange }) => {
+  return (
+    <PermissionManagement value={value} onChange={onChange} />
+  );
+};
+const defaultData: DataSourceType[] = [];
+
+export default () => {
+  const [editableKeys, setEditableRowKeys] = useState<React.Key[]>([]);
+  const [dataSource, setDataSource] = useState<readonly DataSourceType[]>([]);
+
+  const columns: ProColumns<DataSourceType>[] = [
+    {
+      title: 'Vai trò',
+      dataIndex: 'title',
+      tooltip: 'Quyền vào các trang',
+      width: '15%',
+    },
+    {
+      title: 'Quyền',
+      dataIndex: 'labels',
+      width: '75%',
+      valueType: 'formList',
+      renderFormItem: () => <TagList />,
+      render: (_, row) => row?.labels?.map((item) => {
+        if (item.includes('*')) {
+          let i = item.split('*')
+          let color = ''
+          switch (i[1]) {
+            case "Create":
+              color = colors[0]
+              break;
+
+            case "Read":
+              color = colors[1]
+              break;
+
+            case "Update":
+              color = colors[2]
+              break;
+
+            case "Delete":
+              color = colors[3]
+              break;
+            default:
+              console.log("Hành động không được nhận diện");
+          }
+          return <Tag key={item} className={`${color} text-white`}>{i[0] + "-" + i[1]}</Tag>
+        }
+      })
+ 
+    },
+    {
+      title: '',
+      valueType: 'option',
+      width: '10%',
+      render: (__, record, _, action) => [
+        <a
+          key="editable"
+          onClick={() => {
+            action?.startEditable?.(record.id);
+          }}
+        >
+          Sửa
+        </a>,
+        <a
+          key="delete"
+          onClick={() => {
+            setDataSource(dataSource.filter((item) => item.id !== record.id));
+          }}
+        >
+          Xoá
+        </a>,
+      ],
+    },
+  ];
+
+  return (
+    <>
+      <EditableProTable<DataSourceType>
+        rowKey="id"
+        headerTitle="Bảng vai trò"
+        maxLength={5}
+        scroll={{
+          x: 960,
+        }}
+        recordCreatorProps={{
+          position: 'top',
+          record: () => ({ id: (Math.random() * 1000000).toFixed(0) }),
+          creatorButtonText: 'Tạo vai trò mới',
+        }
+        }
+        loading={false}
+        columns={columns}
+        request={async () => ({
+          data: defaultData,
+          total: 3,
+          success: true,
+        })}
+        value={dataSource}
+        onChange={setDataSource}
+        editable={{
+          type: 'multiple',
+          editableKeys,
+          saveText: "Lưu",
+          cancelText: 'Huỷ',
+          onSave: async (rowKey, data, row) => {
+            console.log(rowKey, data, row);
+            await waitTime();
+          },
+          onChange: setEditableRowKeys,
+        }}
+      />
+    </>
+  );
+};
